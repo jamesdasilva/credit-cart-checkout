3478bf53c6ad923001ba94946467fa67d6836f4c
James da Silva <jamesodas@gmail.com>
2020-04-12 19:57:39 -0300

    refactor(PayFormSelect): o componente foi tranformado em uma function (adoção de hooks)

--- src/js/credit-card-checkout/pay-form/pay-form-input-select/pay-form-input-select.js
+++ src/js/credit-card-checkout/pay-form/pay-form-input-select/pay-form-input-select.js
@@ -1,62 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './pay-form-input-select.scss';
 
-class PayFormSelect extends Component {
-	constructor(props) {
-    super(props);
-		this.state = {
-      value: '0',
-      isValid: true
-		};
-    this.updateValue = this.updateValue.bind(this);
-    this.validate = this.validate.bind(this);
-  }
-	updateValue(e) {
-    this.setState({ 
-      value: e.target.value,
-      isValid: this.validate(e.target.value)
-    });
-    this.props.update({
-      name: this.props.name,
-      value: e.target.value,
-      isValid: this.validate(e.target.value)
-    });
-  }
-  validate(value) {
+const PayFormSelect = (props) => {
+  
+  const [state, setState] = useState({
+    value: '0',
+    isValid: true
+  });
+  
+  const validate = (value) => {
     if(value > 0) {
       return true;
     } else {
       return false;
     }  
-  }
-	render() {
-		return (
-      <div className="pay-select" data-testid='pay-select'>
-        <label className={this.state.value != "0" ? "pay-select__label pay-select__label--visible" : "pay-select__label"}>
-          {this.props.label}
-        </label>
-        <div className={this.state.value != "0" ? "pay-select__container" : "pay-select__container pay-select__container--empty"}>
-          <select 
-            data-testid={"input-select"}
-            id={this.props.name}
-            className={this.state.isValid ? "pay-select__select" : "pay-select__select pay-select__select--invalid"}
-            value={this.state.value}
-            onChange={this.updateValue}
-            onBlur={this.validate}
-            placeholder={this.props.label}>
-            <option value="0">{this.props.label}</option>
-            <option value="1">1X</option>
-            <option value="3">3X</option>
-            <option value="5">5X</option>
-            <option value="10">10X</option>
-          </select>
-        </div>
-        <div className={!this.state.isValid ? "pay-select__alert" : "pay-select__alert pay-select__alert--invisible"}>
-          {this.props.invalidMsg}
-          </div>
+  };
+
+	const updateValue = (e) => {
+    setState({ 
+      value: e.target.value,
+      isValid: validate(e.target.value)
+    });
+    props.update({
+      name: props.name,
+      value: e.target.value,
+      isValid: validate(e.target.value)
+    });
+  };
+
+  return (
+    <div className="pay-select" data-testid='pay-select'>
+      <label className={state.value != "0" ? "pay-select__label pay-select__label--visible" : "pay-select__label"}>
+        {props.label}
+      </label>
+      <div className={state.value != "0" ? "pay-select__container" : "pay-select__container pay-select__container--empty"}>
+        <select 
+          data-testid={"input-select"}
+          id={props.name}
+          className={state.isValid ? "pay-select__select" : "pay-select__select pay-select__select--invalid"}
+          value={state.value}
+          onChange={updateValue}
+          onBlur={validate}
+          placeholder={props.label}>
+          <option value="0">{props.label}</option>
+          <option value="1">1X</option>
+          <option value="3">3X</option>
+          <option value="5">5X</option>
+          <option value="10">10X</option>
+        </select>
       </div>
-		);
-	}
+      <div className={!state.isValid ? "pay-select__alert" : "pay-select__alert pay-select__alert--invisible"}>
+        {props.invalidMsg}
+        </div>
+    </div>
+  );
 }
 
 export default PayFormSelect;
