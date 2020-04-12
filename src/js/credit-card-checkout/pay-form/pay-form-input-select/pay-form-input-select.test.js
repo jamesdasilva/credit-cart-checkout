0d6d9192a2714175c38af0b974c891e6a7c9d2aa
James da Silva <jamesodas@gmail.com>
2020-04-12 20:12:23 -0300

    Refactor(PayFormSelect): pequenas melhorias no código de teste

--- src/js/credit-card-checkout/pay-form/pay-form-input-select/pay-form-input-select.test.js
+++ src/js/credit-card-checkout/pay-form/pay-form-input-select/pay-form-input-select.test.js
@@ -4,97 +4,78 @@ import React from 'react'
 
 import PayFormSelect from './pay-form-input-select'
 
+let _mock;
+
+beforeEach(() => {
+  _mock = {
+    name: "installments",
+    label: "Número de parcelas",
+    invalidMsg: "Insira o número de parcelas",
+    update: jest.fn()
+  };
+});
+
+afterEach(() => { });
+
 describe('<PayFormSelect /> spec', () => {
   test('Snapshot test', () => {
-    const _mock = {
-      name: "installments",
-      label: "Número de parcelas",
-      invalidMsg: "Insira o número de parcelas",
-      update: jest.fn()
-    }
     const { getByTestId } = render(
       <PayFormSelect
         name={_mock.invalidMsg}
         label={_mock.label}
         invalidMsg={_mock.invalidMsg}
-        update={_mock.update} />
-      )
+        update={_mock.update} />);
 
-    const payFormSelect = getByTestId('pay-select')
-    expect(payFormSelect).toMatchSnapshot()
+    expect(getByTestId('pay-select')).toMatchSnapshot();
   })
   test('Deve chamar update passando o novo valor quando houver alteração', () => {
-    const _mock = {
-      name: "installments",
-      label: "Número de parcelas",
-      invalidMsg: "Insira o número de parcelas",
-      update: jest.fn()
-    }
     const _paramCall = { 
       name: _mock.name, 
       value: '1', 
       isValid: true 
-    }
+    };
     const { getByTestId } = render(
       <PayFormSelect
         name={_mock.name}
         label={_mock.label}
         invalidMsg={_mock.invalidMsg}
-        update={_mock.update} />
-      )
+        update={_mock.update} />);
 
-    const inputSelect = getByTestId('input-select')
-    fireEvent.change(inputSelect, { target: { value: "1" } });
-    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall)
+    fireEvent.change(getByTestId('input-select'), { target: { value: "1" } });
+    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall);
   })
 
   test('Deve retornar { ...p, isValid: true } quando o valor for igual a 0', () => {
-    const _mock = {
-      name: "installments",
-      label: "Número de parcelas",
-      invalidMsg: "Insira o número de parcelas",
-      update: jest.fn()
-    }
     const _paramCall = { 
       name: _mock.name, 
       value: '0', 
       isValid: false 
-    }
+    };
     const { getByTestId } = render(
       <PayFormSelect
         name={_mock.name}
         label={_mock.label}
         invalidMsg={_mock.invalidMsg}
-        update={_mock.update} />
-      )
+        update={_mock.update} />);
 
-    const inputSelect = getByTestId('input-select')
-    fireEvent.change(inputSelect, { target: { value: "0" } });
-    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall)
+    fireEvent.change(getByTestId('input-select'), { target: { value: "0" } });
+    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall);
   })
 
   test('Deve retornar { ...p, isValid: true } quando o valor for diferente de 0', () => {
-    const _mock = {
-      name: "installments",
-      label: "Número de parcelas",
-      invalidMsg: "Insira o número de parcelas",
-      update: jest.fn()
-    }
     const _paramCall = { 
       name: _mock.name, 
       value: '3', 
       isValid: true 
-    }
+    };
     const { getByTestId } = render(
       <PayFormSelect
         name={_mock.name}
         label={_mock.label}
         invalidMsg={_mock.invalidMsg}
-        update={_mock.update} />
-      )
+        update={_mock.update} />);
 
-    const inputSelect = getByTestId('input-select')
-    fireEvent.change(inputSelect, { target: { value: "3" } });
-    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall)
+    fireEvent.change(getByTestId('input-select'), { target: { value: "3" } });
+    expect(_mock.update.mock.calls[0][0]).toEqual(_paramCall);
   })
 })
