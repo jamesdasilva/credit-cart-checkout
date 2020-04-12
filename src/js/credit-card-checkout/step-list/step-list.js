c5ee34af71056a318213d2b5135af8d2a69dae31
James da Silva <jamesodas@gmail.com>
2020-04-12 15:14:07 -0300

    test(StepList): adiciona testes de unidade

--- src/js/credit-card-checkout/step-list/step-list.js
+++ src/js/credit-card-checkout/step-list/step-list.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle,faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import './step-list.scss';
 
 const StepList = ({steps, stepSelected}) => {
   return (
-    <div className="step-list">
+    <div className="step-list" data-testid='step-list'>
       {steps.map((item, i) => (
         <div className="step-list__item" key={i}>
         { i < steps.findIndex(s => s == stepSelected) ? 
-          (<div className="step-list__item-icon">
+          (<div className="step-list__item-icon" data-testid='check-icon'>
             <FontAwesomeIcon icon={faCheckCircle} size="lg" />
         </div>) : <div className="step-list__number">{i + 1}</div>}
         {item}
@@ -22,6 +23,11 @@ const StepList = ({steps, stepSelected}) => {
       ))}
     </div>
   );
+}
+
+StepList.propTypes = {
+  steps: PropTypes.arrayOf(String),
+  stepSelected: PropTypes.string
 }
 
 export default StepList;
--- src/js/credit-card-checkout/step-list/step-list.test.js
+++ src/js/credit-card-checkout/step-list/step-list.test.js
@@ -1,5 +1,131 @@
+import '@testing-library/jest-dom'
+import { render, cleanup, waitForElement, fireEvent } from '@testing-library/react'
+import React from 'react'
+
 import StepList from './step-list';
 
-test('testando...', () => {
-  expect(3).toBe(3);
-});
+describe('<StepList /> spec', () => {
+  test('Snapshot test', () => {
+    const objectMock = { 
+      steps: [
+        "Carrinho",
+        "Pagamento",
+        "Confirmação"
+      ],
+      stepSelected: "Carrinho"
+    }
+    const { getByTestId } = render(
+      <StepList
+        steps={objectMock.steps}
+        stepSelected={objectMock.stepSelected} />
+      )
+
+    const stepList = getByTestId('step-list')
+    expect(stepList).toMatchSnapshot()
+  })
+
+  test('Deve exibir os nomes das steps nos itens da lista', () => {
+    const objectMock = {
+      steps: [
+        "Carrinho",
+        "Pagamento",
+        "Confirmação"
+      ],
+      stepSelected: "Carrinho"
+    }
+    const { getByTestId } = render(
+      <StepList
+        steps={objectMock.steps}
+        stepSelected={objectMock.stepSelected} />
+      )
+
+    const itemListCount = getByTestId('step-list').childNodes
+    
+    const itemTexts = [...itemListCount].map(item => {
+      return item.textContent.match(/[ A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+/)[0]
+    })
+
+    expect(itemTexts).toEqual(objectMock.steps)
+  })
+
+  test('Deve não exibir check-icon nenhum quando stepSelected == steps[0]', () => {
+    const objectMock = {
+      steps: [
+        "Carrinho",
+        "Pagamento",
+        "Confirmação"
+      ],
+      stepSelected: "Carrinho"
+    }
+    const { queryAllByTestId, queryByText } = render(
+      <StepList
+        steps={objectMock.steps}
+        stepSelected={objectMock.stepSelected} />
+      )
+
+    const itemIcon = queryAllByTestId('check-icon')
+    expect(itemIcon.length).toBeFalsy();
+
+    const item1 = queryByText(/[1]+/)
+    const item2 = queryByText(/[2]+/)
+    const item3 = queryByText(/[3]+/)
+    expect(item1).toBeTruthy();
+    expect(item2).toBeTruthy();
+    expect(item3).toBeTruthy();
+
+  })
+
+  test('Deve exibir check-icon no primeiro item quando stepSelected == steps[1]', () => {
+    const objectMock = {
+      steps: [
+        "Carrinho",
+        "Pagamento",
+        "Confirmação"
+      ],
+      stepSelected: "Pagamento"
+    }
+    const { queryByTestId, queryByText } = render(
+      <StepList
+        steps={objectMock.steps}
+        stepSelected={objectMock.stepSelected} />
+      )
+
+    const itemIcon = queryByTestId('check-icon')
+    expect(itemIcon).toBeTruthy();
+
+    const item1 = queryByText(/[1]+/)
+    const item2 = queryByText(/[2]+/)
+    const item3 = queryByText(/[3]+/)
+    expect(item1).toBeFalsy();
+    expect(item2).toBeTruthy();
+    expect(item3).toBeTruthy();
+
+  })
+
+  test('Deve exibir check-icon nos dois primeiros itens quando stepSelected == steps[2]', () => {
+    const objectMock = {
+      steps: [
+        "Carrinho",
+        "Pagamento",
+        "Confirmação"
+      ],
+      stepSelected: "Confirmação"
+    }
+    const { queryAllByTestId, queryByText } = render(
+      <StepList
+        steps={objectMock.steps}
+        stepSelected={objectMock.stepSelected} />
+      )
+
+    const itemIcon = queryAllByTestId('check-icon')
+    expect(itemIcon.length).toBe(2);
+
+    const item1 = queryByText(/[1]+/)
+    const item2 = queryByText(/[2]+/)
+    const item3 = queryByText(/[3]+/)
+    expect(item1).toBeFalsy();
+    expect(item2).toBeFalsy();
+    expect(item3).toBeTruthy();
+
+  })
+})
